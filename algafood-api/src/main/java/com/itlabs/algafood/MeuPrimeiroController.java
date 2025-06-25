3ea85b9ed6b03c39e39c863e0ada091b1b8b05b3
Ygor Nascimento <ygor.nascimento@github.com>
2025-06-25 08:20:42 -0300

    2.8 - Restart mais rápido da aplicação com DevTools

--- algafood-api/src/main/java/com/itlabs/algafood/MeuPrimeiroController.java
+++ algafood-api/src/main/java/com/itlabs/algafood/MeuPrimeiroController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-
 public class MeuPrimeiroController {
 
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
-        return "hello";
+        return "Olá6!";
     }
 }

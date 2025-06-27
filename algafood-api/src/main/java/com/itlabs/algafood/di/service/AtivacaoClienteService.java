95ebe9a9849e3248a6d9d11618aa88b1ba457487
Ygor Nascimento <ygor.nascimento@github.com>
2025-06-27 07:05:46 -0300

    2.14 - Outra forma de injeção de dependência usando o setter da classe. A Exception lançada é pela falta de comunicar ao Container como essa instância será injetada, já que não há anotação.

--- algafood-api/src/main/java/com/itlabs/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/itlabs/algafood/di/service/AtivacaoClienteService.java
@@ -10,10 +10,10 @@ public class AtivacaoClienteService {
 
     private Notificador notificador;
 
-    @Autowired
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-    }
+//    @Autowired
+//    public AtivacaoClienteService(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 
     /* Quando temos mais de um construtor em uma classe que está marcada para ser um Bean (através do @Component por exemplo),
     o SpringContext (ou ApplicationContext ou Container) não sabe qual construtor usar, e como não encontrou o construtor
@@ -21,13 +21,22 @@ public class AtivacaoClienteService {
     [com.itlabs.algafood.di.service.AtivacaoClienteService]: No default constructor found;). Como dizemos qual construtor
     usar? Usando a anotação @Autowired no construtor que queremos que o Container utilize.
     * */
-    public AtivacaoClienteService(String qualquerCoisa) {
-
-    }
+//    public AtivacaoClienteService(String qualquerCoisa) {
+//
+//    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    }
+
+    /* Outra forma de fazer a injeção de dependência é via o setter da propriedade da classe, porém, sem avisar ao Container
+     (através da anotação @Autowired) que vamos usar o setter também temos um erro, só que agora,  de runtime
+     (java.lang.NullPointerException: Cannot invoke "com.itlabs.algafood.di.notificacao.Notificador.notificar
+     (com.itlabs.algafood.di.modelo.Cliente, String)" because "this.notificador" is null)
+    * */
+    public void setNotificador(Notificador notificador) {
+        this.notificador = notificador;
     }
 }

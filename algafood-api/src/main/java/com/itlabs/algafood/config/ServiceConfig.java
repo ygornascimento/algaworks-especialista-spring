61514ee3d3a9690312c88b39f9242fa7e5f35378
Ygor Nascimento <ygor.nascimento@github.com>
2025-06-27 06:21:57 -0300

    Preparando para aula 2.14

--- algafood-api/src/main/java/com/itlabs/algafood/config/ServiceConfig.java
+++ algafood-api/src/main/java/com/itlabs/algafood/config/ServiceConfig.java
@@ -1,15 +1,15 @@
-package com.itlabs.algafood.config;
-
-import com.itlabs.algafood.di.notificacao.Notificador;
-import com.itlabs.algafood.di.service.AtivacaoClienteService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ServiceConfig {
-
-    @Bean
-    public AtivacaoClienteService atividacaoClienteService(Notificador notificador) {
-        return new AtivacaoClienteService(notificador);
-    }
-}
+//package com.itlabs.algafood.config;
+//
+//import com.itlabs.algafood.di.notificacao.Notificador;
+//import com.itlabs.algafood.di.service.AtivacaoClienteService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ServiceConfig {
+//
+//    @Bean
+//    public AtivacaoClienteService atividacaoClienteService(Notificador notificador) {
+//        return new AtivacaoClienteService(notificador);
+//    }
+//}
--- algafood-api/src/main/java/com/itlabs/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/itlabs/algafood/di/service/AtivacaoClienteService.java
@@ -2,15 +2,15 @@ package com.itlabs.algafood.di.service;
 
 import com.itlabs.algafood.di.modelo.Cliente;
 import com.itlabs.algafood.di.notificacao.Notificador;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
     private Notificador notificador;
 
     public AtivacaoClienteService(Notificador notificador) {
         this.notificador = notificador;
-
-        System.out.println("AtivacaoClienteService: " + notificador);
     }
 
     public void ativar(Cliente cliente) {

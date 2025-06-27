dc180d75b14ba0be3785c96f7f68ebac57ee2a36
Ygor Nascimento <ygor.nascimento@github.com>
2025-06-27 06:47:00 -0300

    2.14 - Erro por falta de definição de qual construtor usar ao fazer dessa classe um Bean no Container.

--- algafood-api/src/main/java/com/itlabs/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/itlabs/algafood/di/service/AtivacaoClienteService.java
@@ -13,6 +13,16 @@ public class AtivacaoClienteService {
         this.notificador = notificador;
     }
 
+    /* Quando temos mais de um construtor em uma classe que está marcada para ser um Bean (através do @Component por exemplo),
+    o SpringContext (ou ApplicationContext ou Container) não sabe qual construtor usar, e como não encontrou o construtor
+    padrão da classe, ele se perde e lança uma exception (org.springframework.beans.BeanInstantiationException: Failed to instantiate
+    [com.itlabs.algafood.di.service.AtivacaoClienteService]: No default constructor found;). Como dizemos qual construtor
+    usar? Usando a anotação @Autowired no construtor que queremos que o Container utilize.
+    * */
+    public AtivacaoClienteService(String qualquerCoisa) {
+
+    }
+
     public void ativar(Cliente cliente) {
         cliente.ativar();
 

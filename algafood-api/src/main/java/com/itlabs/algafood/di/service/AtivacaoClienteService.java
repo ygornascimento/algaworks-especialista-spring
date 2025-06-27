4aa84ed2d67c34ce92c3b5bcc43efc90888caf5f
Ygor Nascimento <ygor.nascimento@github.com>
2025-06-27 07:24:25 -0300

    2.15 - Dependência opcional com @Autowired - Removendo do Bean a classe NotificationEmail.

--- algafood-api/src/main/java/com/itlabs/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/itlabs/algafood/di/service/AtivacaoClienteService.java
@@ -8,37 +8,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired
+    @Autowired(required = false)
     private Notificador notificador;
-
-//    @Autowired
-//    public AtivacaoClienteService(Notificador notificador) {
-//        this.notificador = notificador;
-//    }
-
-    /* Quando temos mais de um construtor em uma classe que está marcada para ser um Bean (através do @Component por exemplo),
-    o SpringContext (ou ApplicationContext ou Container) não sabe qual construtor usar, e como não encontrou o construtor
-    padrão da classe, ele se perde e lança uma exception (org.springframework.beans.BeanInstantiationException: Failed to instantiate
-    [com.itlabs.algafood.di.service.AtivacaoClienteService]: No default constructor found;). Como dizemos qual construtor
-    usar? Usando a anotação @Autowired no construtor que queremos que o Container utilize.
-    * */
-//    public AtivacaoClienteService(String qualquerCoisa) {
-//
-//    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        if (notificador != null) {
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        } else {
+            System.out.println("Não existe notificador, mas cliente foi ativado!");
+        }
     }
-
-    /* Outra forma de fazer a injeção de dependência é via o setter da propriedade da classe, porém, sem avisar ao Container
-     (através da anotação @Autowired) que vamos usar o setter também temos um erro, só que agora,  de runtime
-     (java.lang.NullPointerException: Cannot invoke "com.itlabs.algafood.di.notificacao.Notificador.notificar
-     (com.itlabs.algafood.di.modelo.Cliente, String)" because "this.notificador" is null)
-    * */
-//    @Autowired
-//    public void setNotificador(Notificador notificador) {
-//        this.notificador = notificador;
-//    }
 }

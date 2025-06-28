30b5f7646f8cf8902e0ab6b14696dd55cd113b02
Ygor Nascimento <ygor.nascimento@github.com>
2025-06-28 16:35:23 -0300

    2.16 - Ambiguedade de beans e injeção de lista de beans - Primeiro exemplo: usando uma lista de notificadores e imprimindo os dois.

--- algafood-api/src/main/java/com/itlabs/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/itlabs/algafood/di/service/AtivacaoClienteService.java
@@ -5,19 +5,24 @@ import com.itlabs.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AtivacaoClienteService {
+    /*
+    Como podemos fazer para sanar o problema: (Field notificador in com.itlabs.algafood.di.service.AtivacaoClienteService
+    required a single bean, but 2 were found:)? Existem algumas opções, como a seguir, onde podemos fazer o notificador
+    se tornar uma lista e recebermos os dois.
+    * */
 
-    @Autowired(required = false)
-    private Notificador notificador;
+    @Autowired
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        if (notificador != null) {
+        for (Notificador notificador : notificadores) {
             notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-        } else {
-            System.out.println("Não existe notificador, mas cliente foi ativado!");
         }
     }
 }

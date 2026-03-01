4553080f77e6a8d26fe3ed7fe74487cf04645473
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-03-01 17:13:40 -0300

    Added domain files

--- algafood-api/src/main/java/com/itlabs/algafood/jpa/CadastroCozinha.java
+++ algafood-api/src/main/java/com/itlabs/algafood/jpa/CadastroCozinha.java
@@ -1,4 +1,19 @@
 package com.itlabs.algafood.jpa;
 
+import com.itlabs.algafood.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
 public class CadastroCozinha {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    public List<Cozinha> listar() {
+        return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+    }
 }

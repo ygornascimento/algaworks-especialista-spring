b69b69fbc6da490410eb387c88ee9bf9688d7bcc
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-03-17 05:32:03 -0300

    Implementando Content Negociation para JSON ou XML

--- algafood-api/src/main/java/com/itlabs/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/itlabs/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.itlabs.algafood.api.controller;
 import com.itlabs.algafood.domain.model.Cozinha;
 import com.itlabs.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
     }

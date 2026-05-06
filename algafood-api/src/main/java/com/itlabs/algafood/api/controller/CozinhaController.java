d27cccd389efef4b30ae6efc3741fa9e8806abd3
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-05-05 21:42:52 -0300

    Adicionado PUT na Controller e no .http

--- algafood-api/src/main/java/com/itlabs/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/itlabs/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.itlabs.algafood.api.controller;
 import com.itlabs.algafood.domain.model.Cozinha;
 import com.itlabs.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,14 +24,34 @@ public class CozinhaController {
     }
 
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId) {
-        return  cozinhaRepository.buscar(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+        if (cozinhaId != null) {
+            return ResponseEntity.ok(cozinha);
+        }
+        return  ResponseEntity.notFound().build();
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Cozinha adicionar(@RequestBody Cozinha cozinha) {
         Cozinha cozinhaSalva = cozinhaRepository.salvar(cozinha);
 
         return cozinhaSalva;
+    }
+
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
+                                             @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+
+        if (cozinhaAtual != null) {
+            cozinhaAtual.setNome(cozinha.getNome());
+            cozinhaRepository.salvar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+        }
+        return ResponseEntity.notFound().build();
     }
 }

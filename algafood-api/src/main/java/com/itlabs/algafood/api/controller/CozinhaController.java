4c896b2c01ea844a8ae09a5f29523315bd9e4b77
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-05-14 17:33:40 -0300

    Adicionado a camada de service de Cozinha.

--- algafood-api/src/main/java/com/itlabs/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/itlabs/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.itlabs.algafood.api.controller;
 
 import com.itlabs.algafood.domain.model.Cozinha;
 import com.itlabs.algafood.domain.repository.CozinhaRepository;
+import com.itlabs.algafood.domain.service.CadastroCozinhaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,12 +12,39 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+/*
+* Por que construtor é preferido para DI?
+1) Dependências obrigatórias ficam explícitas
+Quando a Controller precisa de Service para existir, isso aparece no construtor.
+Com injeção em campo, essa obrigação fica “escondida”.
+
+2) Imutabilidade (final)
+Com construtor, você consegue declarar dependências como final, evitando alteração acidental depois da criação do objeto.
+
+3) Melhor testabilidade
+Fica muito mais fácil criar a classe em teste passando mocks pelo construtor, sem precisar de reflection/hacks.
+
+4) Falha mais cedo (fail fast)
+Se faltar dependência, o erro aparece na criação do bean, de forma mais clara.
+
+5) Aderência a princípios OO/SOLID
+Combina melhor com:
+Dependency Inversion
+Design explícito de contratos
+Baixo acoplamento acidental de framework
+*
+* */
+
 @RestController
 @RequestMapping("/cozinhas")
 public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
+
+    @Autowired
+    private CadastroCozinhaService cadastroCozinhaService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
@@ -37,9 +65,7 @@ public class CozinhaController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Cozinha adicionar(@RequestBody Cozinha cozinha) {
-        Cozinha cozinhaSalva = cozinhaRepository.salvar(cozinha);
-
-        return cozinhaSalva;
+        return cadastroCozinhaService.salvar(cozinha);
     }
 
     @PutMapping("/{cozinhaId}")

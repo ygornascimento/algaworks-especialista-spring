6ab15702023d939f78cf152dcfd9ab73c2dc325c
Ygor Nascimento <ygor.nascimento@outlook.com>
2026-05-26 07:32:21 -0300

    4.31. Desafio: Modelando e implementando a atualização de recursos de restaurantes

--- algafood-api/src/main/java/com/itlabs/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/itlabs/algafood/api/controller/RestauranteController.java
@@ -4,6 +4,7 @@ import com.itlabs.algafood.domain.exceprion.EntidadeNaoEncontradaException;
 import com.itlabs.algafood.domain.model.Restaurante;
 import com.itlabs.algafood.domain.repository.RestauranteRepository;
 import com.itlabs.algafood.domain.service.CadastroRestauranteService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,6 +41,23 @@ public class RestauranteController {
             restaurante = cadastroRestauranteService.salvar(restaurante);
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(restaurante);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/restauranteId")
+    public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+        Restaurante restauranteModificado = restauranteRepository.buscar(restauranteId);
+
+        try {
+            if  (restauranteModificado != null) {
+                BeanUtils.copyProperties(restaurante, restauranteModificado, "id");
+
+                restauranteModificado = cadastroRestauranteService.salvar(restauranteModificado);
+                return ResponseEntity.ok(restauranteModificado);
+            }
+            return ResponseEntity.notFound().build();
         } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
